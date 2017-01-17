a7c9977a75ad3002ea9cba6ef4d1010944732daa
Thomas Sonderman <trsonderm@gmail.com>
2017-01-17 09:06:13 -0500

    added observer pattern

---
+++ Design Patterns/Observer/Observer/Observer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public class Observer : IObserver
+    {
+        public string ObserverName { get; private set; }
+        public Observer(string name)
+        {
+            this.ObserverName = name;
+        }
+        public void Update()
+        {
+            Console.WriteLine($"{this.ObserverName} A new product has arrived");
+        }
+    }
+
+    interface IObserver
+    {
+        void Update();
+    }
+}
---
+++ Design Patterns/Observer/Observer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new Subject();
+            Observer observer1 = new Observer("Observer 1");
+            subject.Subscribe(observer1);
+            subject.Subscribe(new Observer("Observer 2"));
+            subject.Inventory++;
+            subject.Unsubscribe(observer1);
+            subject.Subscribe(new Observer("Observer 3"));
+            subject.Inventory++;
+            Console.ReadKey();
+        }
+    }
+   
+}
